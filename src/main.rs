90fb4f622c3694739f09f54632964d1526934471
Stephan Creutz <stephan.cr@gmx.de>
2024-06-08 17:56:24 +0200

    read event from JSON object format as well

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,8 @@ struct EventArg<'a> {
     function_args: Cow<'a, str>,
     #[serde(borrow)]
     location: Option<Cow<'a, str>>,
+    #[serde(borrow)]
+    detail: Option<Cow<'a, str>>,
 }
 
 #[derive(Debug, Deserialize, PartialEq, Serialize)]
@@ -47,15 +49,59 @@ enum EventType {
 struct Event<'a> {
     #[serde(default)]
     args: Option<EventArg<'a>>,
+    /// event category
     #[serde(default)]
     cat: String,
     #[serde(default)]
     #[serde(borrow)]
     name: Cow<'a, str>,
+    /// event type
     ph: EventType,
+    /// process ID
     pid: u32,
+    /// thread ID
     tid: u32,
+    /// timestamp in microseconds
     ts: u32,
+    /// duration in microseconds, exists only for events of type [`EventType::Complete`]
+    dur: Option<u32>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct Top<'a> {
+    #[serde(borrow)]
+    trace_events: Vec<Event<'a>>,
+}
+
+fn process_event<'a>(event: Event<'a>, stack: &mut Vec<Event<'a>>) {
+    if event.ph == EventType::Begin {
+        stack.push(event);
+    } else if event.ph == EventType::End {
+        let trace = stack
+            .iter()
+            .map(|e| e.name.borrow())
+            .collect::<Vec<&str>>()
+            .join(";");
+        if let Some(begin_event) = stack.pop() {
+            println!("{} {}", trace, event.ts - begin_event.ts);
+        }
+    } else if event.ph == EventType::Complete {
+        println!(
+            "{} {} {}",
+            event.name,
+            if let Some(ref a) = event.args {
+                if let Some(ref d) = a.detail {
+                    <Cow<'_, str> as Borrow<str>>::borrow(d)
+                } else {
+                    ""
+                }
+            } else {
+                ""
+            },
+            event.dur.unwrap()
+        );
+    }
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
@@ -66,28 +112,32 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                 .required(true)
                 .value_parser(value_parser!(PathBuf)),
         )
+        .arg(
+            Arg::new("object")
+                .required(false)
+                .long("object")
+                .num_args(0),
+        )
         .get_matches();
 
     if let Some(value) = matches.get_one::<PathBuf>("f") {
         let mut stack = Vec::<Event<'_>>::new();
         let file = File::open(value)?;
-        let deserializer = Deserializer::from_reader(file);
+        let deserializer = Deserializer::from_reader(&file);
 
-        let stream = deserializer.into_iter::<Vec<Event<'_>>>();
-
-        for value in stream {
-            for event in value? {
-                if event.ph == EventType::Begin {
-                    stack.push(event);
-                } else if event.ph == EventType::End {
-                    let trace = stack
-                        .iter()
-                        .map(|e| e.name.borrow())
-                        .collect::<Vec<&str>>()
-                        .join(";");
-                    if let Some(begin_event) = stack.pop() {
-                        println!("{} {}", trace, event.ts - begin_event.ts);
-                    }
+        if matches.contains_id("object") {
+            let deserializer = Deserializer::from_reader(&file);
+            let stream = deserializer.into_iter::<Top<'_>>();
+            for value in stream {
+                for event in value?.trace_events {
+                    process_event(event, &mut stack);
+                }
+            }
+        } else {
+            let stream = deserializer.into_iter::<Vec<Event<'_>>>();
+            for value in stream {
+                for event in value? {
+                    process_event(event, &mut stack);
                 }
             }
         }
